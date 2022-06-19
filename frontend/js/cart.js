f9a10b8d448a82971b027c431602f347c3da1fb2
JulieLCY <wi20b056@technikum-wien.at>
2022-06-19 14:00:21 +0200

    cart

---
+++ frontend/js/cart.js
@@ -0,0 +1,57 @@
+if (document.readyState == "loading" ){
+    document.addEventListener("DOMContentLoaded", ready)
+} else {
+    ready()
+}
+
+function ready(){
+    var removeCartItemButtons = document.getElementsByClassName("text-danger")
+    for (var i = 0; i< removeCartItemButtons.length; i++){
+        var button = removeCartItemButtons[i]
+            button.addEventListener("click", removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName("cart-quantity")
+    for (var i = 0; i< quantityInputs.length; i++){
+        var input = quantityInputs[i]
+        input.addEventListener("change", quantityChanged)
+    }
+
+}
+
+
+function removeCartItem(event){
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotalPrice()
+
+}
+
+function quantityChanged(event){
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0){
+        input.value = 1
+    }
+    updateCartTotalPrice()
+}
+
+
+
+
+function updateCartTotalPrice(){
+    var cartItemContainer = document.getElementsByClassName("cart-item")[0]
+    var cartRows = cartItemContainer.getElementsByClassName("cart-row")
+    var total = 0
+    for (var i = 0; i< cartRows.length; i++){
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName("cart-price")[0]
+        var quantityElement = cartRow.getElementsByClassName("cart-quantity")[0]
+        var price = parseFloat(priceElement.innerText.replace("€", ""))
+        var quantity = quantityElement.value
+        console.log(price * quantity)
+        total = total + (price * quantity)
+
+    }
+    total = Math.round(total * 100)/100
+    document.getElementsByClassName("cart-total-price")[0].innerText = "€" + total
+}
